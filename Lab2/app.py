459c3f170bcb7136255ba096bebf8615df9953b7
Kseniia Ivanchenko <k.ivanchenko-2027@kpi.ua>
2023-12-28 16:59:22 +0100

    change lab 2

--- Lab2/app.py
+++ Lab2/app.py
@@ -1,41 +1,30 @@
 import hashlib
 import random
+import math
 
 
-class Voter:
-    def __init__(self, voter_id, public_key):
-        self.voter_id = voter_id
-        self.public_key = public_key
-        self.vote = None
+def generate_rsa_keypair():
+    # Generate RSA key pair
+    p = 61  # Example prime number, choose larger primes in real-world scenarios
+    q = 53  # Example prime number, choose larger primes in real-world scenarios
+    n = p * q
+    phi = (p - 1) * (q - 1)
 
-    def generate_blind_signature(self, message, private_key):
-        # Emulate the process of generating a blind signature
-        # In a real-world scenario, this would involve the interaction with an authority
-        return pow(message, private_key[0], private_key[1])
+    # Choose e such that 1 < e < phi and gcd(e, phi) = 1
+    e = random.randint(2, phi - 1)
+    while math.gcd(e, phi) != 1:
+        e = random.randint(2, phi - 1)
 
-    def blind_vote(self, candidate_id, public_key):
-        # Blind the vote using the public key of the authority
-        r = random.randint(1, public_key[1] - 1)
-        blinded_vote = (pow(r, public_key[0], public_key[1]) * candidate_id) % \
-                       public_key[1]
-        self.vote = (blinded_vote, r)
+    # Calculate d, the modular multiplicative inverse of e (d * e % phi = 1)
+    d = pow(e, -1, phi)
 
-    def receive_signed_vote(self, signed_blind_vote):
-        # Unblind the signed vote using the voter's private key
-        unblinded_signature = pow(signed_blind_vote, self.public_key[0],
-                                  self.public_key[1])
-        self.vote = (self.vote[0], unblinded_signature)
+    return ((e, n), (d, n))
 
 
-class Authority:
-    def __init__(self):
-        # Authority's RSA key pair generation (normally generated offline)
-        self.private_key = (65537, 1234567890123456789012345678901234567890)  # (e, N)
-        self.public_key = (65537, 1234567890123456789012345678901234567890)  # (d, N)
-
-    def sign_blind_vote(self, blinded_vote):
-        # Sign the blinded vote using the authority's private key
-        return pow(blinded_vote, self.private_key[0], self.private_key[1])
+def blind_sign_message(message, private_key):
+    # Blind sign a message using RSA
+    m = int(hashlib.sha256(str(message).encode()).hexdigest(), 16)
+    return pow(m, private_key[0], private_key[1])
 
 
 def main():
@@ -43,34 +32,37 @@ def main():
     candidates = {1: "Candidate A", 2: "Candidate B"}
     voters = []
 
-    # Create 4 voters
+    # Generate RSA key pairs for voters and authority
+    authority_keypair = generate_rsa_keypair()
     for i in range(4):
-        voter = Voter(voter_id=i + 1,
-                      public_key=(65537, 9876543210987654321098765432109876543210))
-        voters.append(voter)
-
-    authority = Authority()
+        voters.append({
+            'id': i + 1,
+            'keypair': generate_rsa_keypair(),
+            'vote': None
+        })
 
     # Voting phase
     for voter in voters:
         candidate_id = random.choice(list(candidates.keys()))
-        voter.blind_vote(candidate_id, authority.public_key)
-
-        # Authority signs the blinded vote
-        signed_blind_vote = authority.sign_blind_vote(voter.vote[0])
-
-        # Voter receives the signed vote
-        voter.receive_signed_vote(signed_blind_vote)
+        # Blind the vote
+        blinded_vote = pow(candidate_id, voter['keypair'][0][0], voter['keypair'][0][1])
+        # Sign the blinded vote
+        signed_blind_vote = blind_sign_message(blinded_vote, authority_keypair[1])
+        # Store the blinded and signed vote
+        voter['vote'] = (blinded_vote, signed_blind_vote)
 
     # Tally the votes
     tally = {candidate: 0 for candidate in candidates.values()}
     for voter in voters:
         for candidate_id, candidate_name in candidates.items():
-            if voter.vote[0] == candidate_id:
+            # Unblind the vote
+            unblinded_vote = pow(voter['vote'][0], voter['keypair'][1][0],
+                                 voter['keypair'][1][1])
+            if unblinded_vote == candidate_id:
                 # Verify the signature before counting the vote
-                hashed_vote = hashlib.sha256(str(voter.vote[0]).encode()).hexdigest()
-                if pow(int(hashed_vote, 16), authority.public_key[0],
-                       authority.public_key[1]) == voter.vote[1]:
+                hashed_vote = hashlib.sha256(str(voter['vote'][0]).encode()).hexdigest()
+                if pow(voter['vote'][1], authority_keypair[0][0],
+                       authority_keypair[0][1]) == int(hashed_vote, 16):
                     tally[candidate_name] += 1
 
     # Display the results
